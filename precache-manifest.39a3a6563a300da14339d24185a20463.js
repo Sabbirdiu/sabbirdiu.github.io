d7fad5bc06973e7eeb13eda84251b085e4e36732
Sabbir15-8767@diu.edu.bd <sabbir15-8767@diu.edu.bd>
2020-05-29 13:47:52 +0600

    Updates

---
+++ precache-manifest.39a3a6563a300da14339d24185a20463.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f95d2322e6a18554fa8cdff27d26c8",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "9f09cc66e1055f53b119",
+    "url": "/home/static/css/2.e842310e.chunk.css"
+  },
+  {
+    "revision": "209b753fa638766344bd",
+    "url": "/home/static/css/main.7669a19c.chunk.css"
+  },
+  {
+    "revision": "9f09cc66e1055f53b119",
+    "url": "/home/static/js/2.37edd7e3.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/home/static/js/2.37edd7e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209b753fa638766344bd",
+    "url": "/home/static/js/main.97fb6a8e.chunk.js"
+  },
+  {
+    "revision": "380e9afaf55618a537c5",
+    "url": "/home/static/js/runtime-main.bf5ca5dd.js"
+  },
+  {
+    "revision": "d7e51eb387c868f22d80a4043a5838f4",
+    "url": "/home/static/media/pro.d7e51eb3.JPG"
+  },
+  {
+    "revision": "f2ab692be5d2516a8b8090df619a8c78",
+    "url": "/home/static/media/sabbir.f2ab692b.png"
+  },
+  {
+    "revision": "6a5e8fca0ac529122041f66973e9e9e8",
+    "url": "/home/static/media/valy.6a5e8fca.png"
+  }
+]);
