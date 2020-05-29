c65f33df4e262e0dff3874b4dcfcc64c6392bdaf
Sabbir15-8767@diu.edu.bd <sabbir15-8767@diu.edu.bd>
2020-05-29 13:59:22 +0600

    Updates

---
+++ precache-manifest.f84d73538e36b993dc79e32d7926b405.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d339aadf06d5473df85a5d980dd130",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "9f09cc66e1055f53b119",
+    "url": "/resume/static/css/2.e842310e.chunk.css"
+  },
+  {
+    "revision": "06c0a0365cbbe22ed0cb",
+    "url": "/resume/static/css/main.c5f30fd4.chunk.css"
+  },
+  {
+    "revision": "9f09cc66e1055f53b119",
+    "url": "/resume/static/js/2.37edd7e3.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/resume/static/js/2.37edd7e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c0a0365cbbe22ed0cb",
+    "url": "/resume/static/js/main.24b2d4a1.chunk.js"
+  },
+  {
+    "revision": "fc6ccfdad72e7b5d0278",
+    "url": "/resume/static/js/runtime-main.4beab0ad.js"
+  },
+  {
+    "revision": "d7e51eb387c868f22d80a4043a5838f4",
+    "url": "/resume/static/media/pro.d7e51eb3.JPG"
+  },
+  {
+    "revision": "f2ab692be5d2516a8b8090df619a8c78",
+    "url": "/resume/static/media/sabbir.f2ab692b.png"
+  },
+  {
+    "revision": "6a5e8fca0ac529122041f66973e9e9e8",
+    "url": "/resume/static/media/valy.6a5e8fca.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.39a3a6563a300da14339d24185a20463.js"
+  "/resume/precache-manifest.f84d73538e36b993dc79e32d7926b405.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
